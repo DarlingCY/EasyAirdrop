cb8389feb029ef3b8a633457ac7f6e488d6ea0bc
ChenYue <3309168033@qq.com>
2022-11-28 14:45:30 +0800

    init

---
+++ src/main/java/icu/greenlemon/easyairdrop/EasyAirdropApplication.java
@@ -0,0 +1,13 @@
+package icu.greenlemon.easyairdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyAirdropApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyAirdropApplication.class, args);
+    }
+
+}
---
+++ src/test/java/icu/greenlemon/easyairdrop/EasyAirdropApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.greenlemon.easyairdrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyAirdropApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
